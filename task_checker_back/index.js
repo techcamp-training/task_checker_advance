2e10cbd3cae49bc753653cd5948d02ee77139291
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-13 23:35:14 +0900

    cors対応

--- task_checker_back/index.js
+++ task_checker_back/index.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const app = express();
 
+// prismaを読み込む記述
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// cors対策
+const cors = require('cors')
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
+
 // タスクの読み取り処理
 app.get("/tasks", async(req, res) => {
   try {
