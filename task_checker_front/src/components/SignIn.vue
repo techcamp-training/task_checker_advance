215cfe65d8d148110a39d44b2a8239d4276dad20
sh10n7 <shion.yamauchi@di-v.co.jp>
2024-03-16 11:03:53 +0900

    コンポーネントの修正

--- task_checker_front/src/components/SignIn.vue
+++ task_checker_front/src/components/SignIn.vue
@@ -25,7 +25,7 @@ const handleSignIn = async() => {
     <h1>ログイン</h1>
     <input type="text" id="email" v-model="email" placeholder="email">
     <input type="password" id="password"v-model="password" placeholder="password">
-    <button value="新規登録" @click="handleSignIn">新規登録</button>
+    <button value="ログイン" @click="handleSignIn">ログイン</button>
     <p>アカウント作成がお済みでない場合は<router-link to="/signup">こちら</router-link></p>
   </div>
 </template>
